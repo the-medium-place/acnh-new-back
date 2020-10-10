ef1c5b879a626c0d34f1e18b008a51fb80c05c60
ZGS <zgstowell@gmail.com>
2020-10-10 12:28:07 -0700

    jwt update

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require('express-session');
-// var exphbs = require("express-handlebars");
 const cors = require("cors");
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const MongoStore = require('connect-mongo')(session);
+const jwt = require('jsonwebtoken');
+const logger = require('morgan');
+require('dotenv').config();
+// const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT || 3001;
-
 
 const db = require("./models");
 
 const app = express();
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// app.use(express.static("public"));
-
+app.use(logger('dev'));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/acnh_db", {
     useNewUrlParser: true,
@@ -28,36 +26,99 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/acnh_db", {
     useFindAndModify: false
 });
 
+// ====================================
+// BEGIN JWT TRIAL
+// ====================================
+// CHECK FOR JWT TOKEN
+// const authenticateToken = (req, res, next) => {
+//     const authHeader = req.headers['authorizatioin'];
+
+//     // if the header doesn't eist, token will be undefined
+//     const token = authHeader && authHeader.split(' ')[1];
+//     console.log(token);
+
+//     if (!token) return res.status(401).json({ message: 'Invalid token' })
+
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+//         if (err) return res.status(403).json({ message: 'Invalid token' });
+//         // valid token
+//         req.user = user;
+//         next();
+//     })
+// }
+
+const checkAuthStatus = request => {
+    if(!request.headers.authorization) {
+        return false
+    }
+    token = request.headers.authorization.split(' ')[1];
+    // console.log(token);
+    const loggedInUser = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
+        if (err) return false
+        else return data;
+    })
+    // console.log(loggedInUser);
+    return loggedInUser;
+}
+
+
+const generateAccessToken = user => {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: 1000 * 60 * 60
+    })
+}
+
+// =======================================
+// END JWT SETUP
+// =======================================
+
+
+
 // LOCAL DEV LINK
 app.use(cors({
     origin: ["http://localhost:3000"]
 }));
+// app.use(cookieParser());
 
 //Set up express-session to save user sessions
-app.use(session({
-    secret: "keyboard cat",
-    resave: true,
-    // add store method here later!!
-    saveUninitialized: true,
-    cookie: {
-        maxAge: 720000
-    }
-}))
+// app.use(session({
+//     secret: "keyboard cat",
+//     store: new MongoStore({
+//         // mongooseConnection: db,
+//         port: PORT,
+//         collections: 'sessions',
+//         url: process.env.MONGODB_URI || "mongodb://localhost/acnh_db"
+//     }),
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//         maxAge: 2 * 60 * 60 * 1000
+//     },
 
+// }))
+
+// =====================
+// BEGIN ROUTES
+// =====================
 
 //.lean() to make JSON object from Mongoose object
 
 app.get("/api/users", (req, res) => {
     db.User.find({})
-        // .populate("exercises").sort({date:-1}).lean() 
         .then(dbUser => {
             // console.log(dbUser);
-            res.send(dbUser)
+            res.json(dbUser)
         })
         .catch(err => {
             res.json(err);
         });
 })
+// app.use((req, res, next) => {
+//     if (req.cookies.user_sid && !req.session.username) {
+//         res.clearCookie('user_sid');        
+//     }
+//     next();
+// });
 
 app.post("/api/users", ({ body }, res) => {
     // console.log(body)
@@ -68,6 +129,25 @@ app.get("/", (req, res) => {
     res.send("API splash!")
 })
 
+
+app.get("/readsessions", (req, res) => {
+    res.json(req.session);
+    // db.getCollection('sessions').findOne({}).then(dbSession => res.json(dbSession))
+})
+
+app.post("/userFromToken", (req,res) => {
+    // console.log("req.body: ",req.body.token)
+    const loggedInUser = jwt.verify(req.body.token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
+        if(err) {
+            console.log(err);
+            return false
+        }
+        else {return data}
+    })
+    // console.log(loggedInUser);
+    res.json(loggedInUser);
+})
+
 app.post("/login", (req, res) => {
 
     // console.log(req.body.username);
@@ -75,64 +155,114 @@ app.post("/login", (req, res) => {
 
     db.User.findOne({ username: req.body.username })
         .then(dbUser => {
-            console.log(dbUser);
+            // console.log(dbUser);
             if (!dbUser) {
-                req.session.user = false;
                 console.log("could not find user");
-                res.status(404).end();
+                res.json("username not found").status(404).end();
             }
-            if (bcrypt.compareSync(req.body.password, dbUser.password)) {
-                // req.session.username = dbUser.username;
-                // req.session.hemisphere = dbUser.islandHemisphere;
-                // req.session.islaneName = dbUser.islandName
-
-                req.session.user = {
-                    username: dbUser.username,
-                    id: dbUser._id,
-                    islandHemisphere: dbUser.islandHemisphere,
-                    islandName: dbUser.islandName
-                }
+            else if (bcrypt.compareSync(req.body.password, dbUser.password)) {
+                // user logged int
+                // Create JWT
                 console.log("success");
-                console.log(req.session);
-                // res.redirect('/view-events');
-                // res.redirect("/");
-                res.json(req.session).status(200).end();
+                const user = {
+                    username: dbUser.username,
+                    islandHemisphere: dbUser.islandHemisphere,
+                    islandName: dbUser.islandName,
+                    id: dbUser._id
+                };
+
+                const accessToken = generateAccessToken(user);
+ 
+                // send the JWT to the user
+                res.status(200).json({ ...user, accessToken });
+
+                // res.send({ accessToken, refreshToken })
+
             } else {
                 console.log("unsuccess");
-                req.sessions.user = false;
-                res.status(404).end();
-                // res.redirect('/');
+                res.json("please check credentials and try again.").status(404);
             }
-        })
-    // db.User
-    //   .findOne({
-    //     where: {
-    //       username: req.body.username 
-    //     }
-    //   })
-    //   .then((dbUser) => {
-    //     if (dbUser.username === null) {
-    //       console.log("could not find user");
-    //       res.status(404).end();
-    //     }
-    //     if (bcrypt.compareSync(req.body.password, dbUser.password)) {
-    //       req.session.username = dbUser;
-    //       console.log("success");
-    //       // res.redirect('/view-events');
-    //       res.redirect("/view-events");
-    //       res.status(200).end();
-    //     } else {
-    //       console.log("unsuccess");
-    //       res.redirect("/login-fail");
-    //       res.status(404).end();
-    //       // res.redirect('/');
-    //     }
-    //   });
+        }).catch(err => {
+            console.log(err);
+            res.status(500).send('Please try again.');
+        });
 })
 
-app.get('/readsessions', (req,res) => {
-    res.json(req.session);
+// to get new access token if the old one has expired
+// called manually from the client if the user's token is expired
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token;
+    // refreshToken should be saved in DB
+    if (!refreshToken) return res.status(401);
+    if (!refreshTokens.includes(refreshToken)) return res.status(403);
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(403);
+        const accessToken = generateAccessToken({ name: user.name });
+        res.json({ accessToken });
+    });
+});
+
+app.post('/userFromToken', (req,res) => {
+    const loggedInUser = jwt.verify(req.body.token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
+        if (err) {return false}
+        else {return data};
+    })
+    res.json(loggedInUser);
 })
+
+// remove the access token
+app.delete('/logout', (req, res) => {
+    if (!req.body.token) return res.sendStatus(400)
+    // remove from DB
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+    res.sendStatus(204);
+});
+
+
+app.get("/users/:id", (req, res) => {
+    const loggedInUser = checkAuthStatus(req);
+    if(loggedInUser){
+    db.User.findOne({ _id: req.params.id })
+        .then(dbUser => {
+            res.json(dbUser);
+        })
+    } else {
+        res.status(401).send('not logged in');
+    }
+})
+
+
+
+
+
+
+// db.User
+//   .findOne({
+//     where: {
+//       username: req.body.username 
+//     }
+//   })
+//   .then((dbUser) => {
+//     if (dbUser.username === null) {
+//       console.log("could not find user");
+//       res.status(404).end();
+//     }
+//     if (bcrypt.compareSync(req.body.password, dbUser.password)) {
+//       req.session.username = dbUser;
+//       console.log("success");
+//       // res.redirect('/view-events');
+//       res.redirect("/view-events");
+//       res.status(200).end();
+//     } else {
+//       console.log("unsuccess");
+//       res.redirect("/login-fail");
+//       res.status(404).end();
+//       // res.redirect('/');
+//     }
+//   });
+
+
 // TEST USER COLLECTION
 // db.User.create({
 //     username: 'zgstowel',
